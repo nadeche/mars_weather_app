fcbb4e4d83bf9b04cceba82b25cd7397cab2d53f
NadecheStuder <nadeche.studer@gmail.com>
2016-06-02 21:55:25 +0200

    AndoridStudio project added. With weatherData and GraphData activities and actionbar icons.

---
+++ app/src/main/java/mprog/nl/mars_weather_explorer/GraphDataFragment.java
@@ -0,0 +1,27 @@
+package mprog.nl.mars_weather_explorer;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Nadeche on 2-6-2016.
+ */
+public class GraphDataFragment extends android.support.v4.app.Fragment{
+
+    public static GraphDataFragment newInstance(){
+        GraphDataFragment fragment = new GraphDataFragment();
+        return fragment;
+    }
+
+    //public GraphDataFragment{ }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_graph_data, container, false);
+        return rootView;
+    }
+}
